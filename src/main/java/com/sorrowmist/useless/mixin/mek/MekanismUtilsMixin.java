960e269d32adbb55e914a427f7e319d0d2508e43
magcicada <l1334938030@gmail.com>
2026-07-25 21:50:38 +0800

    Update MekanismUtilsMixin.java

--- src/main/java/com/sorrowmist/useless/mixin/mek/MekanismUtilsMixin.java
+++ src/main/java/com/sorrowmist/useless/mixin/mek/MekanismUtilsMixin.java
@@ -24,14 +24,14 @@ public class MekanismUtilsMixin {
     }
 
     @Inject(method = "getEnergyPerTick", at = @At("RETURN"), cancellable = true)
-    private static void modifyEnergyPerTick(IUpgradeTile tile, long def, CallbackInfoReturnable<Long> cir) {
+    private static void modifyEnergyPerTick(IUpgradeTile tile, FloatingLong def, CallbackInfoReturnable<FloatingLong> cir) {
         if (tile.supportsUpgrades()) {
             cir.setReturnValue(MekUtils.ceilToLong(cir.getReturnValue() * MekUtils.electricity(tile)));
         }
     }
 
     @Inject(method = "getMaxEnergy", at = @At("RETURN"), cancellable = true)
-    private static void modifyMaxEnergy(IUpgradeTile tile, long def, CallbackInfoReturnable<Long> cir) {
+    private static void modifyMaxEnergy(IUpgradeTile tile, FloatingLong def, CallbackInfoReturnable<FloatingLong> cir) {
         if (tile.supportsUpgrades()) {
             cir.setReturnValue(MekUtils.ceilToLong(cir.getReturnValue() * MekUtils.capacity(tile)));
         }
